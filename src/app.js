5b2f8f4f31cc2b9e5fc0f2e1788e654dcc6764fb
Irene VS <147383929+iireneVS@users.noreply.github.com>
2023-12-20 21:14:25 +0000

    finalizado

--- src/app.js
+++ src/app.js
@@ -2,10 +2,60 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
+
+function newCard() {
+  let suits = ["♦", "♥", "♠", "♣"];
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  //Genera los índices aleatorios
+
+  let indexSuits = Math.floor(Math.random() * suits.length);
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+
+  //Capturar los elementos .top .number .end
+
+  let top = document.querySelector("#top");
+  let end = document.querySelector("#end");
+  let number = document.querySelector("#number");
+
+  //Asigno lo generado en los innerHTML
+
+  top.innerHTML = suits[indexSuits];
+  end.innerHTML = suits[indexSuits];
+  number.innerHTML = numbers[indexNumbers];
+
+  //Determino el color
+
+  let currentClass =
+    suits[indexSuits] == "♦" || suits[indexSuits] == "♥"
+      ? "text-danger"
+      : "text-dark";
+  top.className = currentClass;
+  end.className = currentClass;
+  number.className = currentClass;
+}
+
+setInterval(() => newCard(), 5000);
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  newCard();
 };
+
+let btnNewCard = document.querySelector("#btnNewCard");
+btnNewCard.addEventListener("click", () => newCard());
